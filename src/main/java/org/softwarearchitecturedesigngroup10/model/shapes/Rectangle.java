8fcd4af2ba3486aa4581519f354abd45cd5be450
sabatop02 <s.paolillo@bistrategy.it>
2025-05-14 17:50:45 +0200

    Classe Rettangolo

--- src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Rectangle.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Rectangle.java
@@ -3,12 +3,12 @@ package org.softwarearchitecturedesigngroup10.model.shapes;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-import java.awt.Shape;
 
-public class Rectangle extends Shape {
+
+public  class Rectangle extends Shape {
     public double width,height;
     public Rectangle(double width, double height, double y, double x, Color borderColor, Color fillColor, boolean isSelected) {
-        super(x,y,borderColor,fillColor,isSelected);
+        super(y, x, borderColor, fillColor, isSelected);
         this.width = width;
         this.height = height;
 
@@ -16,7 +16,16 @@ public class Rectangle extends Shape {
 
     @Override
      void draw(GraphicsContext gc){
+        gc.setFill(fillColor);
+        gc.fillRect(x, y, width, height);
 
+        gc.setStroke(borderColor);
+        gc.strokeRect(x, y, width, height);
+
+        if (isSelected) {
+            gc.setStroke(Color.BLUE);
+            gc.strokeRect(x - 2, y - 2, width + 4, height + 4);
+        }
     }
 
 
