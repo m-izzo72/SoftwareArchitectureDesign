0c71863819ca21e0cb6211f127ef18b11994b0ee
sabatop02 <s.paolillo@bistrategy.it>
2025-05-14 18:05:24 +0200

    Classe Rettangolo

--- src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Rectangle.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Rectangle.java
@@ -28,6 +28,19 @@ public  class Rectangle extends Shape {
         }
     }
 
+    public double getWidth() {
+        return width;
+    }
 
+    public void setWidth(double width) {
+        this.width = width;
+    }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
