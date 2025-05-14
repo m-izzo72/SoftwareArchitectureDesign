28669cc052236a7f7149ee851924ee1bc664acff
pnocerino <p.nocerino@studenti.unisa.it>
2025-05-14 16:57:38 +0200

    Shape Class Completed

--- src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Shape.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Shape.java
@@ -1,4 +1,47 @@
 package org.softwarearchitecturedesigngroup10.model.shapes;
 
-public class Shape {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public abstract class Shape {
+
+    double x, y;
+    Color borderColor;
+    Color fillColor;
+    boolean isSelected;
+
+    public Shape(double y, double x, Color borderColor, Color fillColor, boolean isSelected) {
+        this.y = y;
+        this.x = x;
+        this.borderColor = borderColor;
+        this.fillColor = fillColor;
+        this.isSelected = isSelected;
+    }
+
+    void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+
+    void setShapeBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    void setShapeFillColor(Color fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    public Color getFillColor() {
+        return fillColor;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    abstract void draw(GraphicsContext gc);
+
 }
