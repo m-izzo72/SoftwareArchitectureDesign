61bfa783594331fd303a5f07c0c40a6694699074
Alessandro Maruotto <a.maruotto1@studenti.unisa.it>
2025-05-14 18:12:31 +0200

    Line Class

--- src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Line.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/model/shapes/Line.java
@@ -1,4 +1,54 @@
 package org.softwarearchitecturedesigngroup10.model.shapes;
 
-public class Line {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Line extends Shape{
+    private Color colorStroke, colorFill;
+    private double x2, y2;
+
+    public Line(double x1, double y1, double x2, double y2, Color borderColor, boolean isSelected) {
+        super(y1, x1, borderColor, null, isSelected);
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    @Override
+    void draw(GraphicsContext gc) {
+        gc.setStroke(borderColor);
+        gc.setLineWidth(isSelected ? 3 : 1); // linea più spessa se selezionata
+        gc.strokeLine(x, y, x2, y2);
+    }
+
+    public Color getColorStroke() {
+        return colorStroke;
+    }
+
+    public void setColorStroke(Color colorStroke) {
+        this.colorStroke = colorStroke;
+    }
+
+    public Color getColorFill() {
+        return colorFill;
+    }
+
+    public void setColorFill(Color colorFill) {
+        this.colorFill = colorFill;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
 }
