0d6a451b9af93a7934a5adf6d7640645ae084e70
pnocerino <p.nocerino@studenti.unisa.it>
2025-05-17 13:24:00 +0200

    Factory

--- src/main/java/org/softwarearchitecturedesigngroup10/model/factories/RectangleFactory.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/model/factories/RectangleFactory.java
@@ -1,12 +1,31 @@
 package org.softwarearchitecturedesigngroup10.model.factories;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class RectangleFactory implements ShapeFactory{
 
     @Override
-    public Shape createShape() {
-        return new Rectangle();
+    public Shape createShape(double startX, double startY, double endX, double endY, Color fill, Color stroke, double strokeWidth) {
+        Rectangle rectangle = new Rectangle();
+
+        // Gestisce correttamente il caso in cui l'utente trascina in direzioni diverse
+        double x = Math.min(startX, endX);
+        double y = Math.min(startY, endY);
+        double width = Math.abs(endX - startX);
+        double height = Math.abs(endY - startY);
+
+        rectangle.setX(x);
+        rectangle.setY(y);
+        rectangle.setWidth(width);
+        rectangle.setHeight(height);
+
+        // Imposta lo stile
+        rectangle.setFill(fill);
+        rectangle.setStroke(stroke);
+        rectangle.setStrokeWidth(strokeWidth);
+
+        return rectangle;
     }
 }
--- src/main/java/org/softwarearchitecturedesigngroup10/model/factories/ShapeFactory.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/model/factories/ShapeFactory.java
@@ -3,6 +3,7 @@ package org.softwarearchitecturedesigngroup10.model.factories;
 
 import javafx.scene.shape.Shape;
 
+import javafx.scene.paint.Color;
 public interface ShapeFactory {
-    public Shape createShape();
+    Shape createShape(double startX, double startY, double endX, double endY, Color fill, Color stroke, double strokeWidth);
 }
