a7d1b020343d44e054577070995bb164ae6b9097
m-izzo72 <m.izzo72@studenti.unisa.it>
2025-06-03 11:00:33 +0200

    fixes

--- src/main/java/org/softwarearchitecturedesigngroup10/MainApplication.java
+++ src/main/java/org/softwarearchitecturedesigngroup10/MainApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -32,7 +33,7 @@ public class MainApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
-
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
     }
 
     public static void main(String[] args) {
